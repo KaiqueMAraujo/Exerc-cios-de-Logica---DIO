fa38bdc2ef18b0d2f087189cf55b20ca4e63e048
Kaique Miguel Silva Araujo <kaiquemiguel018@gmail.com>
2025-12-29 19:02:26 -0300

    Criação de classe jogo

---
+++ Classe Jogo.js
@@ -0,0 +1,35 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque = "";
+
+        if (this.tipo === "mago") {
+            ataque = "magia";
+        } else if (this.tipo === "guerreiro") {
+            ataque = "espada";
+        } else if (this.tipo === "monge") {
+            ataque = "artes marciais";
+        } else if (this.tipo === "ninja") {
+            ataque = "shuriken";
+        }
+
+        console.log(`o ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+// Criando objetos (heróis)
+let heroi1 = new Heroi("Merlin", 150, "mago");
+let heroi2 = new Heroi("Arthur", 30, "guerreiro");
+let heroi3 = new Heroi("Lee", 40, "monge");
+let heroi4 = new Heroi("Hanzo", 25, "ninja");
+
+// Chamando o método atacar
+heroi1.atacar();
+heroi2.atacar();
+heroi3.atacar();
+heroi4.atacar();
